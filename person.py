5eb69e9df150d8e2ed2c272a6c7b7b50896bbf7b
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-08 00:49:05 +0000

    more stuff
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@24 219b0e38-3699-0410-9963-91006c18cdb8

--- person.py
+++ person.py
@@ -1,35 +1,37 @@
-class Person:
-    self.friends = []
-    self.username = ''
-    self.first_name = ''
-    self.last_name = ''
-    self.subjects = []
+_user_table = {}
+class User:
+    @staticmethod
+    def exists(username):
+        if username in _user_table:
+            return True
+        else:
+            return False
+    @staticmethod
+    def get(username):
+        if User.exists(username)
+            return User(_user_table[username])
+        else:
+            return Exception('No user: "%s"' % (username))
+    @staticmethod
+    def add(args):
+        if User.exists(args['username'])
+            return Exception('There is already a user called "%s"' % (args['username']))
+        else:
+            _user_table[args['username']] = args
     def __init__(self, args):
-        
-        self.username = args['username']
-        self.first_name = args['firstname']
-        self.last_name = args['lastname']
-        self.email = args['email']
-        #self.last_name = name[1]
-        #self.subjects = list(subjects)
+        self._args = args
+        self._username = args['username']
+        self._first_name = args['firstname']
+        self._last_name = args['lastname']
+        self._email = args['email']
     def __repr__(self):
-        n = self.first_name, self.last_name
-        return "<Person uid:%s name:%s>" % (self.uid, n)
-    # Python 2.6 provides @properties. We're using python 2.5, grr
-    def uid(self):
-        return str(self.uid)
-    def name(self):
-        return (self.first_name,self.last_name)
-    def subjects(self):
-        return tuple(self.subjects)
-    def add_friend(self,uid):
-        return NotImplementedError
-    def remove_friend(self, uid)
-        return NotImplementedError
-    def friends(self):
-        return NotImplementedError
-        
-#example_person1 = Person(0,('James', 'Curran'), ('English', 'Maths', 'Science'))
-#example_person2 = Person(1,('Tara', 'Murphy'), ('Computing', 'Maths'))
-#example_person3 = Person(2,('Alan', 'Su'), ('History', 'Maths'))
-example_person1 = Person({'firstname': 'James', 'lastname':'Curran', 'school': 'usyd', 'email':'god@it.usyd.edu.au'})
+        return "<Person username:%s>" % self._username
+    def get_username(self):
+        return self._username
+    def get_first_name(self):
+        return self._first_name
+    def get_last_name(self):
+        return self._last_name
+
+User.add({'username': 'james', 'firstname': 'James', 'lastname':'Curran', 'school': 'usyd', 'email':'god@it.usyd.edu.au'})
+
