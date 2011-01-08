6b4489bc3cec29de13b80f3a77cae8d839aecea5
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-08 01:17:43 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@31 219b0e38-3699-0410-9963-91006c18cdb8

--- person.py
+++ person.py
@@ -1,3 +1,4 @@
+from hashlib import sha256
 _user_table = {}
 class User:
     @staticmethod
@@ -24,6 +25,8 @@ class User:
         self._first_name = args['firstname']
         self._last_name = args['lastname']
         self._email = args['email']
+        self._school = args['school']
+        self._password_hash = sha256(args['password'])
     def __repr__(self):
         return "<Person username:%s>" % self._username
     def get_username(self):
@@ -32,6 +35,20 @@ class User:
         return self._first_name
     def get_last_name(self):
         return self._last_name
-
-User.add({'username': 'james', 'firstname': 'James', 'lastname':'Curran', 'school': 'usyd', 'email':'god@it.usyd.edu.au'})
+    def get_email(self):
+        return self._email
+    def get_school(self):
+        return self._school
+    def get_password_hash(self):
+        #TODO: this should be encrypted in some shape or form
+        return self._password_hash
+    def set_first_name(self, fname):
+        self._args['firstname'] = fname
+        self._firstname = self._args['firstname']
+   def password_correct(self, password):
+       if sha256(password) == self.get_password_hash():
+           return True
+        else:
+            return False
+User.add({'username': 'james', 'firstname': 'James', 'lastname':'Curran', 'school': 'usyd', 'email':'god@it.usyd.edu.au', 'password': 'ilovejava'})
 
