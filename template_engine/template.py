be9bb7ad303837288755386a33729565703ec3df
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-08 05:56:02 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@46 219b0e38-3699-0410-9963-91006c18cdb8

--- template_engine/template.py
+++ template_engine/template.py
@@ -1,24 +1,77 @@
 import re
 import cgi
+from TemplateException import TemplateException
 
-EXPR_PATTERN = "\{\{(?P<expr>.*?)\}\}|\{\%(?P<tag>.*?)\%\}"
+class Node(object):
+   def __init__(self, parent):
+      self.parent = parent
+   def render(self, context, response):
+      raise NotImplementedError
 
-def render_view(template, context):
-   def sub(match):
+class GroupNode(Node):
+   def __init__(self, parent):
+      super(GroupNode, self).__init__(parent)
+      self.children = []
+   def render(self, context, response):
+      for child in self.children:
+         child.render(context, response)
+   def add_child(self,node):
+      self.children.append(node)
+      
+class ExprNode(Node):
+   def __init__(self, parent, expr):
+      super(ExprNode, self).__init__(parent)
+      self.expr = expr
+   def render(self, context, response):
+      try:
+         output = str(eval(self.expr, {}, context))
+         response.write(cgi.escape(output))
+      except:
+         response.write(self.expr + " does not exist.")
+
+class HTMLNode(Node):
+   def __init__(self, parent, content):
+      super(HTMLNode, self).__init__(parent)
+      self.content = content
+   def render(self, context, response):
+      response.write(self.content)
+
+class IncludeNode(Node):
+   def __init__(self, parent, filepath):
+      super(IncludeNode, self).__init__(parent)
+      self.filepath = filepath
+      self.root = compile_template(self.filepath)
+   def render(self, context, response):
+      self.root.render(context, response)
+
+def compile_template(filepath):
+   root = GroupNode(None)
+   try:
+      contents = open(filepath, 'rU')
+      parse_template(contents.read(), root)
+      contents.close()
+      return root
+   except IOError:
+      raise
+   except:
+      raise# TemplateException("IncludeException: " + filepath)
+
+PATTERN = "\{\{(?P<expr>.*?)\}\}|\{\%(?P<tag>.*?)\%\}"
+
+def parse_template(contents, parent):
+   upto = 0
+   for match in re.finditer(PATTERN, contents):   
+      parent.add_child(HTMLNode(parent, contents[upto:match.start()]))
       if match.group('expr'):
          expr = match.group('expr').strip()
-         try:
-            x = eval(expr, {}, context)
-         except:
-            print "ExpressionError"
-         return cgi.escape(str(x))
+         parent.add_child(ExprNode(parent, expr))
       elif match.group('tag'):
-         print "Tag matched"
          tag = match.group('tag').strip().split()
-         print tag[1]
          if tag[0] == 'include':
-            try:
-               return render_view(open(tag[1], 'rU').read(), context)
-            except:
-               print "IncludeError"
-   return re.sub(EXPR_PATTERN, sub, template)
+            parent.add_child(IncludeNode(parent, tag[1]))      
+      upto = match.end()
+   parent.add_child(HTMLNode(parent, contents[upto:]))
+
+def render_template(filepath,context, response):
+   root = compile_template(filepath)
+   root.render(context, response)
