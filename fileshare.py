d43e7f04c8c5bdd6e8c6fa20fbf3881ee096b2a0
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-08 01:05:43 +0000

    new version of the file uploader - include more information about the file and unique file names.
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@27 219b0e38-3699-0410-9963-91006c18cdb8

--- fileshare.py
+++ fileshare.py
@@ -1,8 +1,11 @@
 from tornado import Server
-dct={"David": ["1.doc", "2.doc", "a.jpg"],
-     "Chris":["3.doc","4.doc","8.jpg"]}
+import os
+import mimetypes
 
-FORM = """
+dct = {"David": ["1.doc", "2.doc", "a.jpg"],
+       "Chris":["3.doc","4.doc","8.jpg"]}
+
+FORM2 = """
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,13 +19,48 @@ FORM = """
 </html>
 """
 
+FORM = """
+<html>
+<head><title>Hello %s</title></head>
+<body>
+<form method="POST" enctype="multipart/form-data">
+First name: <input type="text" name="firstname" />
+<br />
+Last name: <input type="text" name="lastname" />
+<br />
+<input type="file" name="photo" size="20">
+<br />
+Description: <input type="text" name="description" />
+<input type="submit" name="submit" />
+<br />
+</form>
+</body>
+</html>
+"""
+
+def index(response):
+    global photo, content_type
+    firstname = response.get_field('firstname')
+    lastname = response.get_field('lastname')
+    filename, content_type, data = response.get_file('photo')
+    if firstname is None:
+        response.write(FORM)
+        #read from remote host nlp6.it.usyd.edu.au: Connection reset by peer
+    else:
+        extension = mimetypes.guess_extension(content_type)
+        photo = os.path.join('static', 'images', str(counter) + extension)
+        open(photo, 'wb').write(data)
+        fullname = '%s %s' % (firstname, lastname)
+        photo_url = photo.replace("\\", "/") # URLs don't like backslashes
+        response.write(RESULT % (fullname, fullname, photo_url))
+
 def index(response, otherusr):
-        name1 = response.get_field("name")
-        links = "<ul>\n"
-        for i in dct[otherusr]:
-                links += '\t<li><a href="' + i + '">' + i + '</a></li>\n'
-        links += "</ul>"
-        response.write(FORM % (name1, otherusr, links))
+    name1 = response.get_field("name")
+    links = "<ul>\n"
+    for i in dct[otherusr]:
+    links += '\t<li><a href="' + i + '">' + i + '</a></li>\n'
+    links += "</ul>"
+    response.write(FORM % (name1, otherusr, links))
 
 server = Server()
 server.register("/notes/(.*)", index)
---
+++ upload.py
@@ -0,0 +1,81 @@
+from tornado import Server
+
+from time import time
+import os
+import os.path
+import mimetypes
+
+# notice the HTTP method in the form has been changed to POST
+ul_error_mgs = ""
+
+FORM = """
+<html>
+<head><title>File Upload</title></head>
+<body>
+<form method="POST" enctype="multipart/form-data">
+<input type="hidden" name="ul_flag" value="True"/>
+<p><strong>%s</strong></p>
+First name: <input type="text" name="firstname" />
+<br />
+Last name: <input type="text" name="lastname" />
+<br />
+<input type="file" name="to_upload" size="20">
+<br />
+Subject: <input type="text" name="subject" />
+<br />
+Description: <input type="text" name="subject" />
+<br />
+<input type="submit" name="submit" />
+</form>
+</body>
+</html>
+"""
+
+RESULT = """
+<html>
+<head>
+  <title>Hello %s</title>
+</head>
+<body>
+<h1>Hello %s</h1>
+<img src="/%s" />
+</body>
+</html>"""
+
+ul_file = ""
+
+def chk_ul_fields(response):
+    ul_error_mgs = ""
+    filename, content_type, data = response.get_file('to_upload')
+    if response.get_field("ul_flag") == "True":
+        # we did submit a form
+        if filename == None or content_type == None or data == None:
+            ul_error_mgs = "Please select a file to upload."
+        else:
+            upload_page(response)
+            return
+    # we did not submit a form or it's wrong
+    response.write(FORM % (ul_error_mgs))
+def upload_page(response):
+    global ul_file, content_type
+    firstname = response.get_field('firstname')
+    lastname = response.get_field('lastname')
+    filename, content_type, data = response.get_file('to_upload')
+    if firstname is None:
+        response.write(FORM)
+        #read from remote host nlp6.it.usyd.edu.au: Connection reset by peer
+    else:
+        currenttime = str(int(time()))
+        while os.path.exists("static\\files\\" + currenttime):
+            print "OS Path Exists"
+            currenttime = str(int(time()))
+        extension = mimetypes.guess_extension(content_type)
+        ul_file = os.path.join('static', 'files', currenttime + extension)
+        open(ul_file, 'wb').write(data)
+        fullname = '%s %s' % (firstname, lastname)
+        file_url = ul_file.replace("\\", "/") # URLs don't like backslashes
+        response.write(RESULT % (fullname, fullname, file_url))
+
+server = Server()
+server.register("/upload", chk_ul_fields)
+server.run()
