4e22c91e891619c25ff7ff736ae12c451fbe4675
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-08 06:19:05 +0000

    party in user.py
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@50 219b0e38-3699-0410-9963-91006c18cdb8

--- user.py
+++ user.py
@@ -22,43 +22,73 @@ class User:
         else:
             _user_table[args['username']] = args
     def __init__(self, args):
-        self._args = args
-        self._username = args['username']
-        self._first_name = args['firstname']
-        self._last_name = args['lastname']
-        self._email = args['email']
-        self._school = args['school']
-        self._password_hash = sha256(args['password'])
+        self._set_blank() 
+        print args
+
+        for k,v in args.items():
+            print k,v
+            print "self._%s = '%s'" % (k,v)
+            exec "self._%s = '%s'" % (k,v)
+        #self._args = args
+        #self._username = args['username']
+        #self._first_name = args['firstname']   
+        #self._last_name = args['lastname']
+        #self._email = args['email']
+        #self._school = args['school']
+        #self._password_hash = sha256(args['password'])
+    def _set_blank(self):
+        self._args = {}
+        self._username = ''
+        self._firstname = ''
+        self._lastname = ''
+        self._email = ''
+        self._school = ''
+        self._passwordhash = ''
+        self._profilepicpath = ''
     def __repr__(self):
         return "<Person username:%s>" % self._username
     def get_username(self):
         return self._username
     def get_first_name(self):
-        return self._first_name
+        return self._firstname
     def get_last_name(self):
-        return self._last_name
+        return self._lastname
     def get_email(self):
         return self._email
     def get_school(self):
         return self._school
     def get_password_hash(self):
-        #TODO: this should be encrypted in some shape or form
         return self._password_hash.hexdigest()
+    def get_profile_pic_path(self):
+        return self._profilepicpath
+    def set_mutiple(self, args):
+        mapping = {'firstname': self.set_first_name, 'lastname': self.set_last_name, 'email': self.set_email, 'school': self.set_school,
+                   'password': self.set_password}
+        for k,v in args.items():
+            func = mapping[k]
+            func(v)
+    def set_email(self, email):
+        self._args['email'] = email
+        self._email = email
     def set_first_name(self, fname):
         self._args['firstname'] = fname
-        self._firstname = self._args['firstname']
+        self._firstname = fname
     def set_last_name(self, lname):
         self._args['lastname'] = lname
-        self._lastname = self._args['lastname']
+        self._lastname = lname
+    def set_profile_pic_path(self, path):
+        self._args['profilepath'] = path
+        self._profile_pic_path = path
     def set_school(self, school):
         self._args['school'] = school
-        self._school = self._args['school']
+        self._school = school
+    def set_password(self, pword):
+        self._args['password'] = sha256(password).hexdigest()
+        self._password = sha256(password).hexdigest()
     def password_correct(self, password):
         if sha256(password).hexdigest() == self.get_password_hash():
            return True
         else:
             return False
             
-
 User.add({'username': 'james', 'firstname': 'James', 'lastname':'Curran', 'school': 'usyd', 'email':'god@it.usyd.edu.au', 'password': 'ilovejava'})
-
