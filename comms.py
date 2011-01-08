3f5d372f2126b0d2e4b3db88bb4ae1c0cfc6fc0b
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-08 01:49:06 +0000

    lunchtime
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@36 219b0e38-3699-0410-9963-91006c18cdb8

--- comms.py
+++ comms.py
@@ -17,46 +17,47 @@ Post a message: <br /><textarea name="msg"></textarea><input type="hidden" value
 def adapt_datetime(ts):
 	return time.mktime(ts.timetuple())
 
-connection = sqlite3.connect('./commsdb.db')
-cursor = connection.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS "WALL" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "username" TEXT NOT NULL , "target" TEXT NOT NULL , "date" DATETIME NOT NULL , "message" TEXT NOT NULL )')
-class Wall:
+class WallConnection:
 	def __init__(self, current_user, current_Wall):
 		self.current_user = current_user
-		self.current_Wall = current_Wall
-		
+		self.current_Wall = current_Wall	
+		self.connection = sqlite3.connect('./commsdb.db')
+		self.cursor = self.connection.cursor()
+		self.cursor.execute('CREATE TABLE IF NOT EXISTS "WALL" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "username" TEXT NOT NULL , "target" TEXT NOT NULL , "date" DATETIME NOT NULL , "message" TEXT NOT NULL )')
 	def set_wall(self, current_message):
-	
 		sqlite3.register_adapter(datetime.datetime, adapt_datetime)
 		now = datetime.datetime.now()
 		#users[current_Wall].append(current_user+' says ' + response.get_field('msg'))
-    
 		current_message = current_message.replace("&", "&amp;") 
 		current_message = current_message.replace("<", "&lt;")
 		current_message = current_message.replace(">", "&gt;")
 		current_message = current_message.replace('"', "&quot;")
-		
-		cursor.execute('INSERT INTO "WALL" ("username","target","date","message") VALUES ("%s","%s","%s","%s")' % (self.current_user, self.current_Wall, now, current_message))
-		connection.commit()
-		
+		try:
+			self.cursor.execute('INSERT INTO "WALL" ("username","target","date","message") VALUES ("%s","%s","%s","%s")' % (self.current_user, self.current_Wall, now,current_message))
+			self.connection.commit()
+			return True	
+		except sqlite3.OperationalError, e:
+			return e.args[0]
 	def get_wall(self):
 	
-		final = ''
-		data = cursor.execute('SELECT * FROM WALL WHERE WALL.target = "%s" ORDER BY ID DESC' % self.current_Wall)
+		final = []
+		data = self.cursor.execute('SELECT * FROM WALL WHERE WALL.target = "%s" ORDER BY ID DESC' % self.current_Wall)
 		for row in data:
-			final+= row[1] + row[4]
-			final += row[3]+'<br/>'
+			final.append([row[1], row[4], row[3]])
+			
 		
 		return final
 
 		
 
 def _wall(response,current_Wall):
-	w = Wall(response.get_field('username'),current_Wall)
+	w = WallConnection(response.get_field('username'),current_Wall)
 	final = header % (w.current_Wall, w.current_user, w.current_Wall) 
-	response.write(final + w.get_wall())
+	final += str(w.get_wall())
+	
+	response.write(final)
 def _submit(response):
-	w = Wall(response.get_field('current_user'),response.get_field('current_Wall'))
-	w.set_wall(response.get_field('msg'))
+	w = WallConnection(response.get_field('current_user'),response.get_field('current_Wall'))
+	print w.set_wall(response.get_field('msg'))
 	response.redirect('/wall/%s?username=%s' % (w.current_Wall, w.current_user)) 
 	
