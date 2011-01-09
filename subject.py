4b32b47e9f5deb95c9db3cb8294a2acd2540e5c9
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-09 01:06:00 +0000

    Checking in my updated subject stuff with templates included and fixed a logo error
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@81 219b0e38-3699-0410-9963-91006c18cdb8

--- subject.py
+++ subject.py
@@ -1,96 +1,12 @@
 from tornado import Server
-
-FORM="""
-<html>
-<head>
-<title>New Subject</title>
-<body>
-<h1>Subject Creation Field</h1>
-<form method="POST">
-<input type="text" name="subjectname" /> :Subject Name
-<p>
-<input type="radio" name="subject" value=1> Mathematics
-</p>
-<p>
-<input type="radio" name="subject" value=2> Computing
-</p>
-<p>
-<input type="radio" name="subject" value=3> Science
-</p>
-<p>
-<input type="radio" name="subject" value=4> Social Science
-</p>
-<p>
-<input type="radio" name="subject" value=5> English/Philosophy
-</p>
-<p>
-<input type="radio" name="subject" value=6> Languages
-</p>
-<p>
-<input type="radio" name="subject" value=7> Other
-</p>
-<p>
-Amount of Units or Credits
-<input type="radio" name="unit" value="1"> 1
-<input type="radio" name="unit" value="2"> 2
-<input type="radio" name="unit" value="3"> 3
-<input type="radio" name="unit" value="4"> 4
-</p>
-<p>
-Description of the Course
-</p>
-<p>
-<textarea rows="3" cols="50" name="description"> </textarea>
-</p>
-<p>There are 
-60
-character's left</p>
-<p>
-<input type="reset" name="reset" value=Reset>
-<p>
-<input type='submit' value='Submit Selection'>
-</p>
-</form>
-</body>
-</head>
-</html>
-"""
-
-HTML = """
-<html>
-<head>
-<body>
-<p>Category- %s</p>
-<p>Amount of HSC Units- %i </p>
-<p>Overview- %s</p>
-<form method="POST">
-<input type="submit" value="Confirm Subject Creation">
-</form>
-</body>
-</head>
-</html>
-"""
-
-VIEW = """
-<html>
-<head>
-<body>
-<ul>
-<li><a href="http://localhost:8888/subject" + %s></li>
-<li></li>
-<li></li>
-<li></li>
-</ul>
-</body>
-</head>
-</html>
-"""
+from template_engine import template
 
 subjects = {}
+subjectlist=["English", "Maths 2 unit", "Maths Extension 1", "Maths Extension 2"]
 
 def subjectpage(response):
     if response.get_field("subjectname") == None:
-        response.write(FORM)
+        template.render_template("templates/subject_create_template.html",{},response)
     else:
         subjectname = response.get_field("subjectname")
         subjectunits = response.get_field("unit")
@@ -101,9 +17,11 @@ def subjectpage(response):
 
 def viewsubject(response, subjectname):
     info = subjects[subjectname]
-    response.write(HTML % (info[0], int(info[1]), info[2]))
+    template.render_template("templates/subject_view_template.html",{"info1":info[0], "info2":int(info[1]), "info3":info[2]},response)
+    #response.write(HTML % (info[0], int(info[1]), info[2]))
+        
 
 def listsubject(response):
-    response.write(VIEW % (subjectname))
-   
-    
+    template.render_template("templates/subject_list_template.html",{"subject_name":subjectname},response)
+    #response.write(VIEW % (subjectname))
+    targetsubject=response.get_field("subjectselected")
