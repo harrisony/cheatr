cc75d6100b702c747c0232681677b588e37ba728
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 11:07:54 +0000

    Drops table if exists
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@240 219b0e38-3699-0410-9963-91006c18cdb8

--- recreate_comms_db.py
+++ recreate_comms_db.py
@@ -1,6 +1,8 @@
 import sqlite3
 
 connection=sqlite3.connect("commsdb.sqlite")
+connection.execute('DROP TABLE IF EXISTS "WALL"')
+
 connection.execute('CREATE TABLE "WALL" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "author" TEXT NOT NULL , "target" TEXT NOT NULL , "time" INTEGER NOT NULL , "message" TEXT NOT NULL )')
 
 connection.commit()
