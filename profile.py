f44c2598ec9a4fe2219e949eba4ed1b5196a6fd9
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 08:40:06 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@195 219b0e38-3699-0410-9963-91006c18cdb8

--- profile.py
+++ profile.py
@@ -74,7 +74,7 @@ def profile(response, username):
         #response.write(OUTPUT % (username,username, picture,firstname,lastname,email,school,username))
         context = {"title":fullname, "user":username, 'wallorfeed':'wallupdate',
                    'current_User':user.get_username(),'current_Wall':username, "profile_pic_location":picture,
-                   "email":email, "school":school, "css": "profile", "friends":friends.get_friends(username), "User": User,
+                   "email":email, "school":school, "css": "profile", "friends":friends.get_friends(user.get_username()), "User": User,
                    "interests": interests, "about": about, "education":education}
         template.render_template("templates/profile.html", context, response)
         
