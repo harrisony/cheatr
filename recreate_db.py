b6b64a8ab1482221fe499ca58c24f7edbf4ff490
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 06:07:19 +0000

    Added drop tables functionality for File Uploads and Friends Database
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@176 219b0e38-3699-0410-9963-91006c18cdb8

--- recreate_db.py
+++ recreate_db.py
@@ -1,4 +1,13 @@
+import os
 import sqlite3
-
-
 import recreate_users
+
+conn = sqlite3.connect(os.path.join("data", "DB_Files.sqlite"))
+conn.execute("DROP TABLE IF EXISTS Files")
+conn.commit()
+conn.close()
+
+conn = sqlite3.connect(os.path.join("data", "friends.sqlite"))
+conn.execute("DROP TABLE IF EXISTS friends")
+conn.commit()
+conn.close()
