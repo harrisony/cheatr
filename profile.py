e80f214f77cd6136a75745b811499c666fc44813
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 10:36:20 +0000

    issues fixed
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@231 219b0e38-3699-0410-9963-91006c18cdb8

--- profile.py
+++ profile.py
@@ -6,57 +6,12 @@ import friends
 import auth
 from template_engine import template
 
-
-SIGNUP = """
-<html>
-<head><title>SIGN UP NOW!</title></head>
-<body>
-<h1>SIGN UP!</h1>
-<h1>Create a user here</h1>
-<form method="POST" action="signup"> 
-Username: <input type="text" name="username" /><br />
-First Name: <input type="text" name="firstname" /><br />
-Last Name: <input type="text" name="lastname" /><br />
-Email: <input type="text" name="email" /><br />
-Create a Password: <input type="password" name="password" /><br />
-Current School: <input type="text" name="school" /><br />
-<input type="Submit" value="Sign Up" name="submit" />
-</form>
-</body>
-</html>
-"""
-
-UPDATE = """
-<html>
-<head><title>Update Info</title></head>
-<body>
-<h1>Update Info</h1>
-<form method="POST" action="update_info" enctype="multipart/form-data"> 
-<input type="text" value="%s" name="username" hidden="true" /><br />
-First Name: <input type="text" value="%s" name="firstname" /><br />
-Last Name: <input type="text" value="%s" name="lastname" /><br />
-Email: <input type="text" value="%s" name="email" /><br />
-Create a Password: <input type="password" value="" name="password" /><br />
-Current School: <input type="text" value="%s" name="school" /><br />
-<br>
-<br>
-Upload Profile Picture:<br>
-
-<input type="file" name="photo" size="20">
-
-<br>
-<input type="Submit" value="Update" name="submit" />
-</form>
-</body>
-</html>
-"""
-
 def profile(response, username):
     user = User.get(username)
     auth.require_user(response)
     name = auth.get_user(response)
-    if not user: user = name
-    
+    if not user:
+        user = name
     if user is not None:
         firstname = user.get_first_name()
         lastname = user.get_last_name()
@@ -70,11 +25,9 @@ def profile(response, username):
             picture = "/static/images/default_avatar.jpeg"
         print "PICTURE IS: " + picture
         fullname = firstname + " " + lastname
-        print fullname
         
-        #response.write(OUTPUT % (username,username, picture,firstname,lastname,email,school,username))
         context = {"title":fullname, 'wallorfeed':'wallupdate',
-                   'user':user,'current_Wall':user.get_username(), "profile_pic_location":picture,
+                   "username":user.get_username(),'current_Wall':user.get_username(), "profile_pic_location":picture,
                    "email":email, "school":school, "css": "profile", "friends":friends.get_friends(user.get_username()), "User": User,
                    "interests": interests, "about": about, "education":education}
         template.render_template("templates/profile.html", context, response)
@@ -109,33 +62,45 @@ def clean(string):
 photo = ''
 
 def update(response):
-    global photo, content_type
     auth.require_user(response)
     user = auth.get_user(response)
-    name = user.get_username()
+    global photo, content_type
+    username = clean(user.get_username())
     firstname = clean(response.get_field("firstname"))
     lastname = clean(response.get_field("lastname"))
     email = clean(response.get_field("email"))
-    password = clean(response.get_field("password"))
+    #password = clean(response.get_field("password"))
+    password = "gus"
     school = clean(response.get_field("school"))
     filename, content_type, data = response.get_file('photo')
-        
-    if name and content_type:
+    print "content_type: " + str(content_type)
+    if content_type == None:
+        photo_url = user.get_profile_pic_path()
+    else:
         extension = mimetypes.guess_extension(content_type)
-        photo_path = os.path.join('static', 'photos', name + extension)
+        photo_path = os.path.join('static', 'photos', username + extension)
         print "updating.."
         open(photo_path, 'wb').write(data)
         photo_url = '/' + photo_path.replace("\\","/")
         print photo_url
-        user.set_profile_pic_path(photo_url)
-        fullname = '%s %s' % (name, lastname)
-        newUser = {"username": name, "firstname":firstname,"lastname":lastname,
+    if user:
+        #extension = mimetypes.guess_extension(content_type)
+        #photo_path = os.path.join('static', 'photos', username + extension)
+        #print "updating.."
+        #open(photo_path, 'wb').write(data)
+        #photo_url = '/' + photo_path.replace("\\","/")
+        #print photo_url
+        #user.set_profile_pic_path(photo_url)
+        fullname = '%s %s' % (firstname, lastname)
+        newUser = {"username": username, "firstname":firstname,"lastname":lastname,
                    "email":email,"password":password,"school":school, "profilepath":photo_url}
         print "NEW USER: " + str(newUser)
-        user.set_first_name(firstname)
+        #user.set_first_name(firstname)
+        user.set_multiple(newUser)
         
-        response.redirect("/profile/" + name)
+        response.redirect("/profile/")
     else:
+        password = user.get_password_hash()
         firstname = user.get_first_name()
         lastname = user.get_last_name()
         email = user.get_email()
@@ -143,15 +108,6 @@ def update(response):
         school = user.get_school()
         #response.write(UPDATE % (name,firstname,lastname,email,school))
         title = "Updating " + firstname + " " + lastname
-        context = {"title":title, "user":name, "username":name, "firstname":firstname,
-                   "lastname":lastname, "email":email, "school":school,"css":"update"}
+        context = {"title":title, "user":username, "username":username, "firstname":firstname,
+                   "lastname":lastname, "email":email, "school":school,"password":password}
         template.render_template("templates/update.html", context, response)
-
-
-
-   # def index(response):
-    
-
-    #else:
-     #   response.write(RESULT %(username, username, photo_url))
-        
