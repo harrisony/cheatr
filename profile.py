4b1566dd6cf440252f235794e1b2027717aeac8b
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 12:48:28 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@253 219b0e38-3699-0410-9963-91006c18cdb8

--- profile.py
+++ profile.py
@@ -7,9 +7,14 @@ import auth
 from template_engine import template
 
 def profile(response, username):
-    user = User.get(username)
     auth.require_user(response)
     name = auth.get_user(response)
+    if name == None:
+        return
+    if username == "":
+        username = name.get_username()
+    user = User.get(username)
+
     if not user:
         user = name
     if user is not None:
@@ -27,7 +32,7 @@ def profile(response, username):
         fullname = firstname + " " + lastname
         
         context = {"title":fullname, 'wallorfeed':'wallupdate',
-                   "user":user,'current_Wall':user.get_username(), "profile_pic_location":picture,
+                   "username":user.get_username(),'current_Wall':user.get_username(), "profile_pic_location":picture,
                    "email":email, "school":school, "css": "profile", "friends":friends.get_friends(user.get_username()), "User": User,
                    "interests": interests, "about": about, "education":education}
         template.render_template("templates/profile.html", context, response)
