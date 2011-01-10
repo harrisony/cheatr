1d269a4c1ff90ea5a548dc18ed6981e08e4b4cf2
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 00:19:27 +0000

    Subject database should be working now
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@138 219b0e38-3699-0410-9963-91006c18cdb8

--- database_subject.py
+++ database_subject.py
@@ -13,31 +13,35 @@ SAMPLE_DATA = [('2_Unit_Maths','Maths',2,'Cool stuff'),
 ('Biology','Science',2,'.'),
 ('Engineering','Maths',2,'.')]
 
-count_id = 5
-subjects = {"1":["English",2,3,"dfg"], "2":["Maths 2 Unit",2,3,"dfg"], "3":["Maths Extension 1",2,3,"dfg"], "4":["Maths Extension 2",2,3,"dfg"]}
+#count_id = 5
+#subjects = {"1":["English",2,3,"dfg"], "2":["Maths 2 Unit",2,3,"dfg"], "3":["Maths Extension 1",2,3,"dfg"], "4":["Maths Extension 2",2,3,"dfg"]}
 
 def get_subject(subjectsid):
     cursor.execute("SELECT * FROM subjectlist WHERE id = '%s';" % subjectsid)
     result = cursor.fetchall()
+    return result
+             
 def create_subject(name, category, unit, description):
-    global count_id
-    subjects[str(count_id)] = [name, category, unit, description]
-    count_id += 1
+    #global count_id
+    #subjects[str(count_id)] = [name, category, unit, description]
+    #count_id += 1
+    cursor.execute("INSERT INTO subjectlist (subject, category, unit, description) VALUES ('%s', '%s', %i, '%s')" % (name, category, unit, description)) 
 
 def list_of_subjects():
-    return subjects.keys()
+    cursor.execute("SELECT id, subject FROM subjectlist")
+    result = cursor.fetchall()
+    return result
 
 if __name__ == "__main__":
 
-    connection=sqlite3.connect("subject_database.sqlite")
+    connection=sqlite3.connect("subject_database.db")
     cursor=connection.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS subjectlist (
-               id INTEGER AUTO_INCREMENT,
-               subject STRING,
-               category INTEGER,
-               unit INTEGER,
-               description STRING,
-               PRIMARY KEY(id));""")
+               id INTEGER PRIMARY KEY,
+               subject STRING NOT NULL,
+               category INTEGER NOT NULL,
+               unit INTEGER NOT NULL,
+               description STRING NOT NULL);""")
     for subject in SAMPLE_DATA:
         cursor.execute("INSERT INTO subjectlist (subject, category, unit, description) VALUES %s" % repr(subject))
 
