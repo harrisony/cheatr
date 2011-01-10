a6b7418e29c5e41ce0d9542b4a94f5a245769f4c
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 16:29:55 +0000

    fixed change password so that it has the user in the context.
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@299 219b0e38-3699-0410-9963-91006c18cdb8

--- password.py
+++ password.py
@@ -6,11 +6,13 @@ from template_engine import template
 def change_password(response):
     auth.require_user(response)
     user = auth.get_user(response)
+    if user == None:
+        return
     old_password = response.get_field("old_password")
     new_password1 = response.get_field("new_password1")
     new_password2 = response.get_field("new_password2")
     output = ""
-    context = {"title":"Change Your Password", "css":"password"}
+    context = {"title":"Change Your Password", "css":"password", "user":user}
     if old_password:
         if user.password_correct(old_password):
             if new_password1 == new_password2:
@@ -21,5 +23,5 @@ def change_password(response):
                 output = "Your password's did not match"
         else:
             output = "Incorrect password entered"
-    context = {"title":"Change Your Password", "css":"password","output":output}
+    context = {"title":"Change Your Password", "css":"password","output":output, "user":user}
     template.render_template("templates/change_password.html", context, response)
