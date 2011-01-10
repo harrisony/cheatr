d2271db3f739822ba6dadcd8ed28369d049f7604
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 10:54:33 +0000

    fixed recreate_comms_db.py love from harrison
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@236 219b0e38-3699-0410-9963-91006c18cdb8

--- recreate_comms_db.py
+++ recreate_comms_db.py
@@ -1,13 +1,7 @@
 import sqlite3
 
-self.connection = sqlite3.connect('./commsdb.sqlite')
-self.cursor = self.connection.cursor()
-
-
 connection=sqlite3.connect("commsdb.sqlite")
-cursor=connection.cursor()
-cursor.execute("DROP table if exists WALL")
-self.cursor.execute('CREATE TABLE "WALL" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "author" TEXT NOT NULL , "target" TEXT NOT NULL , "time" INTEGER NOT NULL , "message" TEXT NOT NULL )')
+connection.execute('CREATE TABLE "WALL" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "author" TEXT NOT NULL , "target" TEXT NOT NULL , "time" INTEGER NOT NULL , "message" TEXT NOT NULL )')
 
 connection.commit()
-cursor.close() 
+connection.close() 
