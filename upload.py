6d123e428acf5a7f3e380281ff677a582d1d4c8a
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 16:07:50 +0000

    Modified css for fileupload.css added css back into head.html modified template for fileupload.html fixed bugs and implemented radios into fileupload.html
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@294 219b0e38-3699-0410-9963-91006c18cdb8

--- upload.py
+++ upload.py
@@ -4,19 +4,21 @@ from time import time
 import os
 import mimetypes
 import dbfiles
+import dbuser
 import auth
+import database_subject
 ul_error_msg = ""
 ul_file = ""
 user = None
 username = ""
 
-def file_upload(response): 
+def file_upload(response):
     auth.require_user(response)
     user = auth.get_user(response)
     if user == None:
         return
     else:
-        username = user.get_username()
+        username = user.get_username()    
     ul_error_msg = ""
     filename, content_type, data = response.get_file('to_upload')
     if response.get_field("ul_flag") == "True":
@@ -30,15 +32,22 @@ def file_upload(response):
             return
     # we did not submit a form or it's wrong
     print "loading page first time"
-    context = {"css": "fileupload", "title": "File Uploader", "ul_err_msg": ul_error_msg, "user":user}
+    listofsubjects = []
+    for item in user.get_subjects():
+        listofsubjects.append([item, database_subject.get_subject(item).get_name().replace("_", " ")])
+        print "item", item
+    context = {"css": "fileupload",
+               "title": "File Uploader",
+               "ul_err_msg": ul_error_msg,
+               "ulsubjects": listofsubjects,
+               "ulcategories": ["Notes", "Exam Paper","Exercises", "Solution"],
+               "user": user}
     template.render_template("templates/fileupload.html", context, response)
 
 def _do_upload(response):
     global ul_file, content_type
     sbjct = response.get_field('subject')
-    categor = response.get_field('categor')
-    ###############
-    categor = "Notes"
+    categor = response.get_field('category')
     descr = response.get_field('description')
     filename, content_type, data = response.get_file('to_upload')
     currenttime = str(int(time()))
@@ -64,7 +73,6 @@ def file_search(response):
     else:
         username = user.get_username()
 
-
 def file_edit(response, fileid):
     auth.require_user(response)
     user = auth.get_user(response)
@@ -87,7 +95,7 @@ def listmyfiles(response):
         return
     else:
         username = user.get_username()
-    context = {"allfiles": dbfiles.getFilesUser("124"), "user": user}
+    context = {"allfiles": dbfiles.getFilesUser("124")}
     print context
     template.render_template("templates/listallfiles.html", context, response)
     
