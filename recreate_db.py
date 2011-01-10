4b8c8f3dab5cc101d0505afbae3850684ff22d1c
ncss112 <ncss112@219b0e38-3699-0410-9963-91006c18cdb8>
2011-01-10 08:18:51 +0000

    conflict resolution
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss112@191 219b0e38-3699-0410-9963-91006c18cdb8

--- recreate_db.py
+++ recreate_db.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import recreate_users
+import recreate_subjects_db
 
 conn = sqlite3.connect(os.path.join("data", "DB_Files.sqlite"))
 conn.execute("DROP TABLE IF EXISTS Files")
@@ -11,3 +12,4 @@ conn = sqlite3.connect(os.path.join("data", "friends.sqlite"))
 conn.execute("DROP TABLE IF EXISTS friends")
 conn.commit()
 conn.close()
+
